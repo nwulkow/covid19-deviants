81738b06b829f73b9dd119a6b4ce2e74a01f2581
Felix Schran <fschran@gmail.com>
2020-03-20 23:21:43 +0100

    Setup cookiecutter structure.

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='Identification of COVID19 deviant districts in Germany. #wirvsvirus',
+    author='wirvsvirus',
+    license='MIT',
+)
